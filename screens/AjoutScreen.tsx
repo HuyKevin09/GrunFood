39a21db51aed6474b0e3cfa1372591cb2cfe38e0
K.H <huy.kevin.09@gmail.com>
2022-06-20 17:38:00 +0200

    Profile dropdown

--- screens/AjoutScreen.tsx
+++ screens/AjoutScreen.tsx
@@ -2,14 +2,28 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import DropDownPicker from 'react-native-dropdown-picker'
+import {useState} from "react";
+import {SearchBar} from "@rneui/themed";
 
 export default function AjoutScreen() {
     return (
         <View style={styles.container}>
+            <Searchbar/>
             <Text style={styles.title}>Ajout</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         </View>
 );
+}
+
+const Searchbar = () => {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    return (
+        <View style={styles.container}>
+            <SearchBar/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
--- screens/ProfilScreen.tsx
+++ screens/ProfilScreen.tsx
@@ -2,17 +2,54 @@ import {StyleSheet} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import {useState} from "react";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export default function ProfilScreen() {
-    // @ts-ignore
     return (
         <View style={styles.container}>
+            <DropDown/>
             <Text style={styles.title}>Profil</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         </View>
     );
 }
 
+const DropDown = () => {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    return (
+        <View style={styles.container}>
+            <DropDownPicker
+                setValue={setValue}
+                value={value}
+                items={[
+                    {label: "Végétarien", value: "vegetarien"},
+                    {label: "Végan", value: "vegan"},
+                    {label: "Pesco-végétarien", value: "pv"},
+                    {label: "Flexitarien", value: "flexitarien"},
+                    {label: "Sans gluten", value: "gluten"},
+                    {label: "Sans lactose", value: "lactose"},
+                ]}
+                placeholder={"Sélectionnez votre type d'alimentation"}
+                open={open}
+                setOpen={setOpen}
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                }}
+                containerStyle={{
+                    marginTop: 50,
+                    width: 300,
+                    height: 100,
+                }}
+                labelStyle={{
+                    fontSize: 20,
+                }}
+            />
+        </View>
+    )
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
